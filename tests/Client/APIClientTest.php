efc3c4368de7215240ddc3c044411d23d8905250
Jason Irish <jirish@dynamicagency.com>
2019-07-19 11:29:41 -0500

    add placeholder test

---
+++ tests/Client/APIClientTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dynamic\Foxy\API\Test\Client;
+
+use SilverStripe\Dev\SapphireTest;
+
+class APIClientTest extends SapphireTest
+{
+    /**
+     * @var string
+     */
+    protected static $fixture_file = '../fixtures.yml';
+
+    /**
+     *
+     */
+    public function testConstruct()
+    {
+        $this->markTestSkipped();
+    }
+}
